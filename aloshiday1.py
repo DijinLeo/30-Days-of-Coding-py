42962d5ce5c5f87751a067c77e76dc5a2eaf16ed
aloshitom10 <aloshitomshijo2028@cs.sjcetpalai.ac.in>
2024-11-20 21:37:37 +0530

    Add files via upload

---
+++ aloshiday1.py
@@ -0,0 +1,25 @@
+#fist program
+char = 'A'
+print(char)
+
+
+#second program
+num = 42
+print(num)
+
+
+#third program
+x = 3.14
+print(x)
+
+
+#fourth program
+a = input("Enter any value: ")
+print(a)
+
+
+#fifth program
+integer = int(input("Enter an integer: "))
+float = float(input("Enter a float: "))
+print("Integer value:", integer)
+print("Float value:", float)
